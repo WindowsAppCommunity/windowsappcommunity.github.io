6d5355553d675a1ba9b081966adf4cf575150ca0
Emiliano Magliocca <emiliano.magliocca@outlook.com>
2019-07-26 19:36:03 +0400

    Updates

---
+++ precache-manifest.99e70ced5055cdbf266dce6e8ff47b20.js
@@ -0,0 +1,18 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "28bfa715d17de83bc3dd7b8a9bcbe779",
+    "url": "/UwpCommunityProjects/index.html"
+  },
+  {
+    "revision": "281da7399696fc39bb8f",
+    "url": "/UwpCommunityProjects/static/js/2.33f8a18a.chunk.js"
+  },
+  {
+    "revision": "93c27b3f8ceb27ba07e0",
+    "url": "/UwpCommunityProjects/static/js/main.8f6bea6a.chunk.js"
+  },
+  {
+    "revision": "47c7879e44e2e3219ad8",
+    "url": "/UwpCommunityProjects/static/js/runtime~main.38fdb5e0.js"
+  }
+]);
