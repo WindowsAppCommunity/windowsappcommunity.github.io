4d8921179e9eae337d918b21e3812e14996bd113
Emiliano Magliocca <emiliano.magliocca@outlook.com>
2019-07-27 16:23:47 +0400

    static files

---
+++ build/precache-manifest.227376f1aa64d38561225ebe0498f02d.js
@@ -0,0 +1,18 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3ab633146075e1cb223d8c0444e68d67",
+    "url": "/UwpCommunityProjects/index.html"
+  },
+  {
+    "revision": "7a117293803afe72a3e5",
+    "url": "/UwpCommunityProjects/static/js/2.1b73afd1.chunk.js"
+  },
+  {
+    "revision": "c02dc2789e2b1a9ca3d9",
+    "url": "/UwpCommunityProjects/static/js/main.dd845eb7.chunk.js"
+  },
+  {
+    "revision": "47c7879e44e2e3219ad8",
+    "url": "/UwpCommunityProjects/static/js/runtime~main.38fdb5e0.js"
+  }
+]);
