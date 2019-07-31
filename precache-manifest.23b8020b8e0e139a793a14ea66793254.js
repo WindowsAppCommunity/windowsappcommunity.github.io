fcbc45072f11723193a95f3e1a0a6cff390341d0
Emiliano Magliocca <emiliano.magliocca@outlook.com>
2019-07-31 12:28:49 +0400

    Updates

---
+++ precache-manifest.23b8020b8e0e139a793a14ea66793254.js
@@ -0,0 +1,18 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "defe8459a65fefe9d9bd4f33b2640811",
+    "url": "/index.html"
+  },
+  {
+    "revision": "b21f649bc05e7fa8f0fd",
+    "url": "/static/js/2.31feb17d.chunk.js"
+  },
+  {
+    "revision": "f29a461e83d2598da9ce",
+    "url": "/static/js/main.540af2b4.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  }
+]);
