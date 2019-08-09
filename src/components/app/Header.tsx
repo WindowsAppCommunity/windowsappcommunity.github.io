626a11df4e2f4710c5755a9a8130c2e94e13417e
Arlo Godfrey <arlo.godfrey@outlook.com>
2019-08-08 19:23:17 -0500

    Header cleanup

--- src/components/app/Header.tsx
+++ src/components/app/Header.tsx
@@ -10,26 +10,10 @@ const itemStyles: React.CSSProperties = {
 export const Header: React.StatelessComponent = () => {
   return (
     <Stack>
-
       <Stack horizontal horizontalAlign="center">
-        <Stack horizontal horizontalAlign="space-between" style={{ width: "80%" }} verticalAlign="end">
-          <span style={itemStyles}>
-            <Link href="/"><Image src={uwpCommunityLogo} /></Link>
-          </span>
-          <span style={itemStyles}></span>
-          <span style={itemStyles}>
-            <Link href={discordLink} target="_blank">
-              <Image src={discordLogo} />
-            </Link>
-            <Link href={githubLink} target="_blank">
-              <Image src={githubLogo} style={{ marginLeft: 10 }} />
-            </Link>
-            <Link href={mediumLink} target="_blank">
-              <Image src={mediumLogo} style={{ marginLeft: 10 }} />
-            </Link>
-          </span>
-
-        </Stack>
+        <Link href="/">
+          <Image src={uwpCommunityLogo} />
+        </Link>
       </Stack>
       <header style={{ fontFamily: "Segoe UI, Sans-Serif", fontWeight: "lighter", fontSize: "24px" }}>The unofficial UWP Community Discord Server </header>
     </Stack>
