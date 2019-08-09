47cda0e295ccd2517ea8769d4e06761c381f75f3
Arlo Godfrey <arlo.godfrey@outlook.com>
2019-08-08 19:31:56 -0500

    Fixed a link & small cleanups

--- src/components/app/Home.tsx
+++ src/components/app/Home.tsx
@@ -1,7 +1,6 @@
 import React, { CSSProperties } from "react";
 import { Stack, Link } from "office-ui-fabric-react";
-import { Image } from "office-ui-fabric-react/lib/Image";
-import { discordLink, githubLink, mediumLink, uwpCommunityLogoBottom } from "../../common/const";
+import { discordLink, githubLink, mediumLink } from "../../common/const";
 import { Depths } from "@uifabric/fluent-theme/lib/fluent/FluentDepths";
 
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
@@ -21,39 +20,42 @@ const LinkCardStyle: CSSProperties = {
 
 export const Home: React.StatelessComponent = () => {
   return (
-    <Stack horizontalAlign="center" style={{ marginTop: 30 }}>
-      <Stack padding="10px">
-        <Stack style={LinkCardStyle} horizontal verticalAlign="center" horizontalAlign="center" gap="7px">
-          <Stack gap="10px" style={{ margin: "10px" }}>
-            <div>Our server is home to a variety of apps, developers and enthusiasts</div>
-            <Link href={discordLink} target="_blank">
-              <div>Join our discord server</div>
-            </Link>
-          </Stack>
-          <FontAwesomeIcon style={FaIconStyle} icon={["fab", "discord"]} />
+    <Stack padding="10px">
+
+
+      <Stack style={LinkCardStyle} horizontal verticalAlign="center" horizontalAlign="center" gap="7px">
+        <Stack gap="10px" style={{ margin: "10px" }}>
+          <div>Our server is home to a variety of apps, developers and enthusiasts</div>
+          <Link href={discordLink} target="_blank">
+            <div>Join our discord server</div>
+          </Link>
         </Stack>
+        <FontAwesomeIcon style={FaIconStyle} icon={["fab", "discord"]} />
+      </Stack>
 
 
-        <Stack style={LinkCardStyle} horizontal verticalAlign="center" horizontalAlign="center" gap="7px">
-          <FontAwesomeIcon style={FaIconStyle} icon={["fab", "github"]} />
-          <Stack gap="10px" style={{ margin: "10px" }}>
-            <div>Many of our projects are open source, so others can learn or build with them</div>
-            <Link href={discordLink} target="_blank">
-              <div>See our open source projects</div>
-            </Link>
-          </Stack>
-        </Stack>
-
-        <Stack style={LinkCardStyle} horizontal verticalAlign="center" horizontalAlign="center" gap="7px">
-          <Stack gap="10px" style={{ margin: "10px" }}>
-            <div>As a community of talented developers, each year we Launch our projects together in an annnual event known as <code>Launch</code></div>
-            <Link href={mediumLink} target="_blank">
-              <div>Read about Launch 2019</div>
-            </Link>
-          </Stack>
-          <FontAwesomeIcon style={FaIconStyle} icon={["fab", "medium"]} />
+      <Stack style={LinkCardStyle} horizontal verticalAlign="center" horizontalAlign="center" gap="7px">
+        <FontAwesomeIcon style={FaIconStyle} icon={["fab", "github"]} />
+        <Stack gap="10px" style={{ margin: "10px" }}>
+          <div>Many of our projects are open source, so others can learn or build with them</div>
+          <Link href={githubLink} target="_blank">
+            <div>See our open source projects</div>
+          </Link>
         </Stack>
       </Stack>
-    </Stack >
+
+
+      <Stack style={LinkCardStyle} horizontal verticalAlign="center" horizontalAlign="center" gap="7px">
+        <Stack gap="10px" style={{ margin: "10px" }}>
+          <div>As a community of talented developers, each year we Launch our projects together in an annnual event known as <code>Launch</code></div>
+          <Link href={mediumLink} target="_blank">
+            <div>Read about Launch 2019</div>
+          </Link>
+        </Stack>
+        <FontAwesomeIcon style={FaIconStyle} icon={["fab", "medium"]} />
+      </Stack>
+
+    </Stack>
+
   );
 };
