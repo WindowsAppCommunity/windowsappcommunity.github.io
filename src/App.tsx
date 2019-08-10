228538f00751c3afdeb9d610f1306654d3e478ef
Arlo Godfrey <arlo.godfrey@outlook.com>
2019-08-10 10:30:41 -0500

    Move home to Views, remove unused Footer component

--- src/App.tsx
+++ src/App.tsx
@@ -2,7 +2,7 @@ import * as React from "react";
 import { Route, Switch, HashRouter } from 'react-router-dom';
 
 import { Projects } from './views/Projects';
-import { Home } from './components/app/Home';
+import { Home } from './views/Home';
 import { Header } from "./components/app/Header";
 
 import { Stack } from "office-ui-fabric-react";
