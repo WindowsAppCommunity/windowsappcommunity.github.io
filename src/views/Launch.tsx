f9ef9774ef1a6487a0c2e9f66c8e1f3c31dde5a6
Arlo Godfrey <arlo.godfrey@outlook.com>
2019-08-15 09:15:10 -0500

    Refactored some things, fleshed out Launch Page

--- src/views/Launch.tsx
+++ src/views/Launch.tsx
@@ -1,20 +1,70 @@
-import React, { useState } from "react";
-import { Text, Stack } from "office-ui-fabric-react";
+import React, { useState, CSSProperties } from "react";
+import { Text, Stack, Button, IconButton } from "office-ui-fabric-react";
+import { Route } from "react-router";
+import { NavLink } from 'react-router-dom';
+import styled from 'styled-components';
 
-export const Launch = () => {
-    const [signedIn, setSignedIn] = useState(false);
+import HoverBox from '../components/HoverBox';
 
+import { Participants } from "../views/launch/participants";
+import { Signup } from "../views/launch/signup";
+
+export const Launch: React.StatelessComponent<any> = ({ match }: any) => {
     return (
-        (signedIn ? <p>Signed in</p> : SignIn)
+        <Stack>
+            <Route path={`${match.path}/participants`} component={Participants} />
+            <Route path={`${match.path}/signup`} component={Signup} />
+            <Route exact path={match.path} component={LaunchViewSelection} />
+        </Stack>
     );
 };
 
-const SignIn = () => {
+const LaunchViewSelection = () => {
     return (
-        <Stack horizontal horizontalAlign="center">
-            <Text variant="large" style={{ padding: 10 }}>
-                Sign in with Discord
-            </Text>
+        <Stack>
+            
+            <Stack horizontal wrap>
+                <LaunchCard header="Participating apps" description="See which apps are participating in Launch 2020" path="/launch/participants" />
+
+                <LaunchCard header="Submit your app" description="Want to Launch your app with the community?" buttonText="Sign up now" path="/launch/signup" />
+            </Stack>
         </Stack>
+    );
+};
+
+interface ILaunchCardProps {
+    header: string;
+    description: string;
+    path: string;
+    buttonText?: string;
+};
+const LaunchCardStyle = styled(HoverBox)`
+    max-height: 500px;
+    max-width: 300px;
+    padding: 50px;
+    margin: 50px;
+    :hover {
+        pointer: cursor;
+    };
+`;
+
+const LaunchCard: React.FC<ILaunchCardProps> = (props: ILaunchCardProps) => {
+    return (
+        <LaunchCardStyle>
+            <Stack>
+                <Stack horizontal tokens={{ childrenGap: 5 }}>
+                    <Text variant="xLarge">{props.header}</Text>
+                    <NavLink to={props.path}>
+                        {
+                            props.buttonText ?
+                                <Button style={{ marginLeft: "10px" }} text={props.buttonText} />
+                                :
+                                <IconButton primary iconProps={{ iconName: 'Go' }} />
+                        }
+                    </NavLink>
+                </Stack>
+                <Text variant="medium">{props.description}</Text>
+            </Stack>
+        </LaunchCardStyle>
     )
 };
---
+++ src/views/launch/participants.tsx
@@ -0,0 +1,8 @@
+import { Text, Stack, Button } from "office-ui-fabric-react";
+import React, { useState, CSSProperties } from "react";
+
+export const Participants = () => {
+    return (
+        <Text>Participants</Text>
+    )
+};
