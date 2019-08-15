612590dda3643dc041b0f59a0f6739308f171bbc
Arlo Godfrey <arlo.godfrey@outlook.com>
2019-08-15 09:55:30 -0500

    Add hero image / card

--- src/views/Launch.tsx
+++ src/views/Launch.tsx
@@ -1,11 +1,11 @@
 import React, { useState, CSSProperties } from "react";
-import { Text, Stack, Button, IconButton } from "office-ui-fabric-react";
+import { Text, Stack, Button, IconButton, Image, ImageCoverStyle, ImageFit } from "office-ui-fabric-react";
 import { Route } from "react-router";
 import { NavLink } from 'react-router-dom';
 import styled from 'styled-components';
 
 import HoverBox from '../components/HoverBox';
-
+import { Images } from "../common/const";
 import { Participants } from "../views/launch/participants";
 import { Signup } from "../views/launch/signup";
 
@@ -21,12 +21,21 @@ export const Launch: React.StatelessComponent<any> = ({ match }: any) => {
 
 const LaunchViewSelection = () => {
     return (
-        <Stack>
-            
-            <Stack horizontal wrap>
+        <Stack tokens={{childrenGap: 25}}>
+            <HoverBox>
+                <Stack horizontal wrap>
+                    <Image width="100%" height="400px" src={Images.launchAppsHero} coverStyle={ImageCoverStyle.landscape} imageFit={ImageFit.cover} />
+                    <Stack style={{margin: "20px"}}>
+                        <Text variant="xLargePlus">//Launch</Text>
+                        <Text variant="large">An annual event where a community of Windows App developers release their UWP-related projects</Text>
+                        <Text style={{marginTop: "10px"}}>Our Discord server provides direct, 2 way user feedback from users, and a place for newbies to ask questions and learn from those with more experience, creating the perfect environment for apps to grow into something more</Text>
+                    </Stack>
+                </Stack>
+            </HoverBox>
+            <Stack horizontal wrap horizontalAlign="center" tokens={{childrenGap: 25}}>
                 <LaunchCard header="Participating apps" description="See which apps are participating in Launch 2020" path="/launch/participants" />
 
-                <LaunchCard header="Submit your app" description="Want to Launch your app with the community?" buttonText="Sign up now" path="/launch/signup" />
+                <LaunchCard header="Submit your app" description="Want to Launch your app with the community?" buttonText="Sign up now" path="/launch/signup" />                    
             </Stack>
         </Stack>
     );
@@ -42,11 +51,10 @@ const LaunchCardStyle = styled(HoverBox)`
     max-height: 500px;
     max-width: 300px;
     padding: 50px;
-    margin: 50px;
     :hover {
-        pointer: cursor;
-    };
-`;
+                pointer: cursor;
+        };
+    `;
 
 const LaunchCard: React.FC<ILaunchCardProps> = (props: ILaunchCardProps) => {
     return (
