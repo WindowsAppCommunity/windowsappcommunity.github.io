fb775aa058d35a4155196d4feb0b58b4a61723ff
Arlo Godfrey <arlo.godfrey@outlook.com>
2019-08-15 04:42:32 -0500

    Added base participantRequest example and parsing

--- launch/participants/post.js
+++ launch/participants/post.js
@@ -1,6 +1,67 @@
-// This endpoint submits an app to review for launch 2020, it does not modify the actual participants list
+// This endpoint submits an app to review for Launch, it does not modify the actual participants list
 let db = require("./dbclient");
+const currentLaunchYear = 2020;
+
+/**
+ * Example participant request obj
+ */
+let exampleParticipantRequest = {
+    appName: "App",
+    author: "SomeDev",
+    description: "Brief app description",
+    isPrivate: true,
+    contact: {
+        email: "", // Optional
+        discord: "" // Required, server presence should be verified by discord bot
+    }
+};
 
 module.exports = (req, res) => {
+    const body = req.body;
+    const bodyCheck = checkBody(body);
 
+    if (!bodyCheck || bodyCheck[0] === false) {
+        res.status(422);
+        res.json(JSON.stringify({
+            error: "Malformed request",
+            reason: `Parameter "${bodyCheck[1]}" not provided or malformed`
+        }));
+        return;
+    }
+    submitParticipant(body, results => {
+        res.end(results);
+    });
 };
+
+function checkBody(body) {
+    if (!body.appName) return [false, "appName"];
+    if (!body.author) return [false, "author"];
+    if (!body.description) return [false, "description"];
+    if (!body.isPrivate != undefined) return [false, "isPrivate"];
+    if (!body.contact || !body.contact.discord) return [false, "contact"];
+    return true;
+}
+
+function submitParticipant(participantData, cb) {
+    db.query(`select * from launch${currentLaunchYear}submissions`, (err, queryResults) => {
+        if (err.toString().includes("does not exist")) {
+            // create the table
+            createTable(participantData, result => {
+                cb(result);
+            });
+        }
+    });
+}
+
+function createTable(participantData, cb) {
+    db.query(`CREATE TABLE launch${currentLaunchYear} (
+        appname VARCHAR,
+        author VARCHAR,
+        description VARCHAR,
+        private int(1),
+        
+    )`,
+        (err, queryResults) => {
+            cb(queryResults);
+        });
+}
