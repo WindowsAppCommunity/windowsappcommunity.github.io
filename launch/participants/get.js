1a007fa060a431bdbb7ee44cd0bd621525f57280
Arlo Godfrey <arlo.godfrey@outlook.com>
2019-08-15 01:48:55 -0500

    Add shutdown handlers and data caching

--- launch/participants/get.js
+++ launch/participants/get.js
@@ -1,10 +1,10 @@
 const { Client } = require('pg');
-
 const client = new Client({
     connectionString: process.env.DATABASE_URL,
     ssl: true,
 });
 
+//#region Shutdown handlers
 process.on('exit', exitHandler.bind(null, { cleanup: true }));
 process.on('SIGINT', exitHandler.bind(null, { exit: true }));
 
@@ -14,17 +14,20 @@ function exitHandler(options, exitCode) {
 
     if (options.exit) process.exit();
 }
-
-client.connect();
+//#endregion
 
 module.exports = (req, res) => {
-    queryDb(`select table_name from information_schema.tables`, results => {
+    getLaunchCached(results => {
         res.end(results);
     });
 };
 
-function queryDb(query, cb) {
-    client.query(query, (err, queryResults) => {
+function createTable() {
+
+}
+
+function getLaunchTable(cb) {
+    client.query(`select table_name from information_schema.tables`, (err, queryResults) => {
         if (err) {
             console.error(err);
             client.end();
@@ -35,6 +38,44 @@ function queryDb(query, cb) {
             results.push(JSON.stringify(row));
         }
         client.end();
+
+        fs.writeFile(launchCachePath, results, () => { }); // Cache the results
         cb(JSON.stringify(results));
     });
 }
+
+// Get and cache the list of launch participants
+// This API is our only surface for interacting with the database, so the cache should be updated when a new participant is added
+const fs = require("fs");
+const launchCacheFilename = "launchCache.json";
+const launchCachePath = __dirname + "/" + launchCacheFilename;
+
+function getLaunchCached(cb) {
+    fs.readdir(__dirname, (err, fileResults) => {
+
+        // If missing, get data from database and create the cache
+        if (!fileResults.includes(launchCacheFilename)) {
+            console.info("Data not cached, refreshing from DB");
+            getLaunchTable(result => {
+                fs.writeFile(launchCachePath, result, () => { });
+                cb(result);
+            });
+            return;
+        }
+
+        // If the file exists, get the contents
+        fs.readFile(launchCachePath, (err, file) => {
+            let fileContents = file.toString();
+
+            if (fileContents.length <= 5) {
+                // Retry
+                getLaunchCached(cb);
+                return;
+            }
+            cb(fileContents);
+        });
+    });
+}
+
+
+client.connect();
