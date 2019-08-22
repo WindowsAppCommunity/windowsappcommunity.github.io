2b57cf8c1721457e91c34ce29c44de7072225d53
Arlo Godfrey <arlo.godfrey@outlook.com>
2019-08-21 21:48:43 -0500

    Moving login button to app header

--- src/components/AppHeader.tsx
+++ src/components/AppHeader.tsx
@@ -1,17 +1,36 @@
 import * as React from "react";
-import { Stack, Link } from "office-ui-fabric-react";
+import { Stack, Link, Text, Button, PrimaryButton } from "office-ui-fabric-react";
 import { Image } from "office-ui-fabric-react/lib/Image";
 import { Images } from "../common/const";
 import { NavMenu } from "./NavMenu";
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { CSSProperties } from "react";
+
+const FaIconStyle: CSSProperties = {
+  color: "white",
+  height: "20px",
+  width: "20px",
+  paddingLeft: "10px"
+};
 
 export const AppHeader: React.StatelessComponent = () => {
   return (
     <header>
-      <Stack style={{ margin: "25px" }} horizontal wrap verticalAlign='end' horizontalAlign="center" tokens={{ childrenGap: 10 }}>
-        <Link href="/">
-          <Image src={Images.uwpCommunityLogo} />
-        </Link>
-        <NavMenu />
+      <Stack style={{ margin: "25px" }} horizontal wrap tokens={{ childrenGap: 10 }} horizontalAlign="space-between">
+        <Stack horizontal wrap verticalAlign='end' horizontalAlign="center" >
+
+          <Link href="/">
+            <Image src={Images.uwpCommunityLogo} />
+          </Link>
+          <NavMenu />
+        </Stack>
+
+        <Stack verticalAlign="end" style={{ marginBottom: "12px" }}>
+          <PrimaryButton>
+            <Text>Sign in </Text>
+            <FontAwesomeIcon style={FaIconStyle} icon={["fab", "discord"]} />
+          </PrimaryButton>
+        </Stack>
       </Stack>
     </header>
   );
