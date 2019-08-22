8ad79d53b0f990fd97896d6aa00f734f9eb92b4f
Arlo Godfrey <arlo.godfrey@outlook.com>
2019-08-22 18:37:56 -0500

    Change CORS setting

--- src/index.ts
+++ src/index.ts
@@ -24,7 +24,7 @@ app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
 app.use((req: Request, res: Response, next: NextFunction) => {
     // Website you wish to allow to connect
-    res.setHeader('Access-Control-Allow-Origin', 'uwpcommunity.github.io');
+    res.setHeader('Access-Control-Allow-Origin', '*');
 
     // Request methods you wish to allow
     res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
