68c5e875a4b1fd7c20fdf1a7d9b364f70f379295
Arlo Godfrey <arlo.godfrey@outlook.com>
2019-08-26 05:13:13 -0500

    Updates

--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.f6ad640ed3b78a1454ffb660904a55e9.js"
+  "/precache-manifest.e2186973896fcce9a9ea6f72a4951343.js"
 );
 
 self.addEventListener('message', (event) => {
