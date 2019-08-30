6cf96f5e19b05aec5d805703552b63f4df8992c6
Emiliano Magliocca <emiliano.magliocca@outlook.com>
2019-08-30 17:31:41 +0300

    - fixed submitParticipant db insert - fixed participants table columns

--- src/launch/participants/submit/post.ts
+++ src/launch/participants/submit/post.ts
@@ -3,7 +3,7 @@ import { IQueryResult } from "../dbclient";
 
 // This endpoint submits an app to review for Launch, it does not modify the actual participants list
 let db = require("../dbclient");
-const currentLaunchYear = 2020;
+const currentLaunchYear = 1; //2020
 
 interface IParticipantContact {
     email?: string;
@@ -40,30 +40,21 @@ function checkBody(body: IParticipantRequest) {
     if (!body.author) return [false, "author"];
     if (!body.description) return [false, "description"];
     if (body.isPrivate == undefined) return [false, "isPrivate"];
-    if (!body.contact || !body.contact.discord) return [false, "contact"];
+    if (!body.contact) return [false, "contact"];
+    // if (!body.contact || !body.contact.discord) return [false, "contact"];
     return true;
 }
 
 function submitParticipant(participantData: IParticipantRequest, cb: Function) {
-    // db.query(`select * from launch${currentLaunchYear}submissions`, (err: Error, queryResults: IQueryResult) => {
-    //     if (err.toString().includes("does not exist")) {
-    //         // create the table
-    //         createTable(participantData, (result: IQueryResult) => {
-    //             cb(result);
-    //         });
-    //     }
-    // });
+    const text = 'INSERT INTO participants(app_name, author, description, is_private, contact, year_id) VALUES($1, $2, $3, $4, $5, $6) RETURNING *';
+    const values = [participantData.appName, participantData.author, participantData.description, 
+                    participantData.isPrivate, participantData.contact, currentLaunchYear];
+    // callback
+    db.query(text, values, (err: Error, queryResults: IQueryResult) => {
+        if (err) {
+            console.log(err.stack)
+        } else {
+            cb(JSON.stringify(queryResults.rows[0]));
+        }
+    })
 }
-
-// function createTable(participantData: IParticipantRequest, cb: Function) {
-//     db.query(`CREATE TABLE launch${currentLaunchYear} (
-//         appname VARCHAR,
-//         author VARCHAR,
-//         description VARCHAR,
-//         private int(1),
-        
-//     )`,
-//         (err: Error, queryResults: IQueryResult) => {
-//             cb(queryResults);
-//         });
-// }
