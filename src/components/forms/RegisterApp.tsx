cebefac8b648ff9b2aa51b35934b4db57afbd18c
Arlo Godfrey <arlo.godfrey@outlook.com>
2019-10-01 01:09:44 -0500

    Reorganized app registration fields into pivots

--- src/components/forms/RegisterApp.tsx
+++ src/components/forms/RegisterApp.tsx
@@ -1,4 +1,4 @@
-import { Text, Stack, PrimaryButton, Checkbox, TextField, DefaultButton, IComboBoxOption, ComboBox } from "office-ui-fabric-react";
+import { Text, Stack, PrimaryButton, Checkbox, TextField, DefaultButton, IComboBoxOption, ComboBox, MaskedTextField, Pivot, PivotItem } from "office-ui-fabric-react";
 import React from "react";
 import { IBackendReponseError } from "../../common/interfaces";
 import { CreateProject, ICreateProjectsRequestBody } from "../../common/services/projects";
@@ -47,50 +47,64 @@ export const RegisterAppForm = (props: IRegisterAppProps) => {
     }
 
     return (
-        <Stack horizontalAlign="center" tokens={{ childrenGap: 10 }}>
+        <Stack horizontalAlign="center">
             {/* Need to toggle both src and display so it trigger the animation, and space is taken up during the transition (while the svg loads) */}
             <img style={{ display: (showSuccessIndicator ? "block" : "none"), height: "200px" }} src={showSuccessIndicator ? "/assets/img/checkanimated.svg" : ""} alt="Check" />
             <Stack horizontalAlign="start" tokens={{ childrenGap: 10 }} style={{ maxWidth: "100%", width: "300px", display: (!showSuccessIndicator ? "block" : "none") }}>
-                <TextField label="App name:"
+                <Pivot>
+                    <PivotItem headerText="Basic info">
+                        <Stack tokens={{ childrenGap: 10 }}>
+                            <TextField label="Project name:" maxLength={50}
                     styles={{ root: { width: "100%" } }}
                     required onChange={(e: any, value: any) => setProjectRequest({ ...projectRequest, appName: value })} />
 
-                <TextField label="Description"
+                            <TextField label="Description" maxLength={140}
                     styles={{ root: { width: "100%" } }}
                     multiline required autoAdjustHeight
-                    placeholder="Enter a brief description of your project"
+                                placeholder="Enter a brief description"
                     onChange={(e: any, value: any) => setProjectRequest({ ...projectRequest, description: value })} />
 
-                <Checkbox label="Project is private"
-                    onChange={(e: any, value: any) => setProjectRequest({ ...projectRequest, isPrivate: value })} />
-
-                <TextField label="Download Link:"
+                            <TextField label="Hero image"
+                                type="url"
+                                styles={{ root: { width: "100%" } }}
+                                required
+                                placeholder="External link to an image of your app"
+                                onChange={(e: any, value: any) => setProjectRequest({ ...projectRequest, externalLink: value })} />
+                        </Stack>
+                    </PivotItem>
+                    <PivotItem headerText="Project links">
+                        <Stack tokens={{ childrenGap: 10 }}>
+                            <MaskedTextField label="Download Link:"
                     styles={{ root: { width: "100%" } }}
                     onChange={(e: any, value: any) => setProjectRequest({ ...projectRequest, downloadLink: value })} />
 
-                <TextField label="Github Link:"
+                            <MaskedTextField label="Github Link:"
                     styles={{ root: { width: "100%" } }}
                     onChange={(e: any, value: any) => setProjectRequest({ ...projectRequest, githubLink: value })} />
 
-                <TextField label="External link"
+                            <MaskedTextField label="External link"
                     styles={{ root: { width: "100%" } }}
                     onChange={(e: any, value: any) => setProjectRequest({ ...projectRequest, externalLink: value })} />
 
+                        </Stack>
+                    </PivotItem>
+                    <PivotItem headerText="More info">
+                        <Stack tokens={{ childrenGap: 10 }}>
                 <ComboBox
-                    label="Your role"
-                    options={roleOptions}
-                    onChange={(e: any, value: any) => setProjectRequest({ ...projectRequest, role: value.text })} />
-
-                <ComboBox
-                    label="Category" hidden
+                                label="Category"
                     options={categoryOptions}
                     onChange={(e: any, value: any) => setProjectRequest({ ...projectRequest, category: value.text })} />
 
+                            <Checkbox label="Project is private/secret"
+                                onChange={(e: any, value: any) => setProjectRequest({ ...projectRequest, isPrivate: value })} />
 
                 <Checkbox label="Partipation in Launch 2020"
                     onChange={(e: any, value: any) => setProjectRequest({ ...projectRequest, awaitingLaunchApproval: value })} />
 
-                <Text style={{ display: projectRequest.awaitingLaunchApproval ? "block" : "none" }}>You will receive a notification once your app is approved for Launch 2020</Text>
+                            <Text style={{ display: projectRequest.awaitingLaunchApproval ? "block" : "none" }}>You will receive a notification once your app is manually approved</Text>
+                        </Stack>
+                    </PivotItem>
+                </Pivot>
 
                 <Text style={{ color: "red" }}>{submissionError}</Text>
                 <Stack horizontal tokens={{ childrenGap: 10 }}>
