b1508bfa1e71a0f4096833917c669ae4593afd60
Arlo Godfrey <arlo.godfrey@outlook.com>
2019-10-01 20:15:51 -0500

    Added no projects message

--- src/views/Projects.tsx
+++ src/views/Projects.tsx
@@ -1,5 +1,5 @@
 import * as React from "react";
-import { Stack, Text } from "office-ui-fabric-react";
+import { Stack, Text, FontIcon } from "office-ui-fabric-react";
 import { GetAllProjects, IProject } from "../common/services/projects";
 import { ProjectCard } from "../components/ProjectCard";
 
@@ -16,11 +16,16 @@ export const Projects: React.StatelessComponent = () => {
   return (
     /* Todo: Add a header with brief explanation of the below */
     <Stack horizontalAlign="center" horizontal wrap tokens={{ childrenGap: 10 }}>
-      {projects ? projects.map(project => {
+      {projects && projects.length > 0 ? projects.map(project => {
         return (
           <ProjectCard project={project}></ProjectCard>
         )
-      }) : <> </>}
+      }) :
+        <Stack horizontalAlign="center" style={{ marginTop: "25vh" }}>
+          <FontIcon iconName="sad" style={{ fontSize: 55 }}></FontIcon>
+          <Text variant="xLarge">No projects, yet</Text>
+        </Stack>
+      }
     </Stack>
   );
 };
