1c0c3290004eadc22d52bdbb8de2e4c06946f004
Arlo Godfrey <arlo.godfrey@outlook.com>
2019-10-09 05:08:19 -0500

    Updates

---
+++ precache-manifest.a0114b642acac016493059f08e34afe8.js
@@ -0,0 +1,18 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6626c567d8093bc4cfbbe8f47c2e94e6",
+    "url": "/index.html"
+  },
+  {
+    "revision": "6611a654de25bc1ad1fa",
+    "url": "/static/js/2.5c21cc84.chunk.js"
+  },
+  {
+    "revision": "2cfe790fdeffb384d49d",
+    "url": "/static/js/main.72971c90.chunk.js"
+  },
+  {
+    "revision": "7c59439c08a835aa0b86",
+    "url": "/static/js/runtime-main.1e5c44f0.js"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.a218e033c11d7cfb4761de31b9d0faef.js"
+  "/precache-manifest.a0114b642acac016493059f08e34afe8.js"
 );
 
 self.addEventListener('message', (event) => {
