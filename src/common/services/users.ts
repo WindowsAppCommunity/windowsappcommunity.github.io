6a263ddcdda3a0004bf7deb9d12f3e881924d649
Arlo Godfrey <arlo.godfrey@outlook.com>
2019-10-12 15:31:57 -0500

    Updated with new endpoint URLs

--- src/common/services/users.ts
+++ src/common/services/users.ts
@@ -1,4 +1,5 @@
 import { fetchBackend } from "../helpers";
+import { IProject } from "./projects";
 
 export async function CreateUser(userData: IUser): Promise<Response> {
     return await fetchBackend("user", "POST", userData);
@@ -9,9 +10,12 @@ export async function ModifyUser(userData: IUser) {
 }
 
 export async function GetUser(discordId: string) {
-    return await fetchBackend(`user?discordId=${discordId}`, "GET");
+    return await fetchBackend(`user/${discordId}`, "GET");
 }
 
+export async function GetUserProjects(discordId: string): Promise<IProject[]> {
+    return (await fetchBackend(`user/${discordId}/projects`, "GET")).json();
+}
 export interface IUser {
     name: string;
     discordId: string;
