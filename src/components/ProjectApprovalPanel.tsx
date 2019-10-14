37fbca18b264190b3b9815357b1e78bc8847c929
Arlo Godfrey <arlo.godfrey@outlook.com>
2019-10-14 01:04:19 -0500

    Use Promise visualizer

--- src/components/ProjectApprovalPanel.tsx
+++ src/components/ProjectApprovalPanel.tsx
@@ -2,21 +2,19 @@ import * as React from "react";
 import { IProject, GetAllProjects_Unfiltered } from "../common/services/projects";
 import { Stack, Text } from "office-ui-fabric-react";
 import { ProjectCard } from "./ProjectCard";
+import { PromiseVisualizer } from "./PromiseVisualizer";
 
 export const ProjectApprovalPanel = () => {
     const [viewModel, setViewModel] = React.useState<IProject[]>([]);
 
-    React.useEffect(() => {
-        GetAllProjects_Unfiltered()
-            .then(setViewModel)
-    });
-
     return (
         <Stack horizontalAlign="center" tokens={{ childrenGap: 20 }}>
             <Text variant="xLarge">Pending review</Text>
-            <Stack wrap horizontal tokens={{ childrenGap: 15 }} horizontalAlign="space-evenly">
-                {viewModel.map(project => { if (project.needsManualReview) return <ProjectCard project={project} /> })}
-            </Stack>
+            <PromiseVisualizer promise={GetAllProjects_Unfiltered()} onResolve={setViewModel} loadingMessage='Loading...' loadingStyle={{ marginTop: "25vh" }} errorStyle={{ marginTop: "25vh" }}>
+                <Stack wrap horizontal tokens={{ childrenGap: 15 }} horizontalAlign="space-evenly">
+                    {viewModel.map(project => { if (project.needsManualReview) return <ProjectCard project={project} /> })}
+                </Stack>
+            </PromiseVisualizer>
         </Stack>
     )
 };
