fbc1ef32e895cf3343e0a14c1f200862ae1b7699
Arlo Godfrey <arlo.godfrey@outlook.com>
2019-10-17 18:53:51 -0500

    Only update data if unset (prevent render loop)

--- src/components/ProjectApprovalPanel.tsx
+++ src/components/ProjectApprovalPanel.tsx
@@ -1,24 +1,38 @@
 import * as React from "react";
 import { IProject, GetAllProjects_Unfiltered } from "../common/services/projects";
-import { Stack, Text } from "office-ui-fabric-react";
+import { Stack, Text, FontIcon } from "office-ui-fabric-react";
 import { ProjectCard } from "./ProjectCard";
 import { PromiseVisualizer } from "./PromiseVisualizer";
 
+interface IProjectApprovalPanelState {
+    promise: Promise<IProject[]>;
+    data?: IProject[];
+    onPromiseFullfilled?: () => {}
+}
+
 export const ProjectApprovalPanel = () => {
-    const [viewModel, setViewModel] = React.useState<IProject[]>([]);
+    const [state, setState] = React.useState<IProjectApprovalPanelState>({ promise: GetAllProjects_Unfiltered() });
+
+    function setApproveableProjects(proj: IProject[]) {
+        if(!state.data) setState({ ...state, data: proj.filter(proj => proj.needsManualReview) });
+    }
 
     return (
         <Stack horizontalAlign="center" tokens={{ childrenGap: 20 }}>
             <Text variant="xLarge">Pending review</Text>
-            <PromiseVisualizer promise={GetAllProjects_Unfiltered()} onResolve={setViewModel} loadingMessage='Loading...' loadingStyle={{ marginTop: "25vh" }} errorStyle={{ marginTop: "25vh" }}>
-                <Stack wrap horizontal tokens={{ childrenGap: 15 }} horizontalAlign="space-evenly">
-                    {viewModel.map(project => {
-                        if (project.needsManualReview) return (
-                            <ProjectCard modOptions editable project={project} />
+            <Stack wrap horizontal tokens={{ childrenGap: 10 }}>
+                <PromiseVisualizer promise={state.promise} onResolve={setApproveableProjects} loadingMessage='Loading Projects...' loadingStyle={{ marginTop: "25vh" }} errorStyle={{ marginTop: "25vh" }}>
+                    {state && (state.data && state.data.length > 0 ? state.data.map((project, i) => (
+                        <ProjectCard modOptions editable key={i} project={project}></ProjectCard>
+                    )) : (
+                            <Stack horizontalAlign="center" >
+                                <FontIcon iconName="sad" style={{ fontSize: 55 }}></FontIcon>
+                                <Text variant="xLarge">No projects, yet</Text>
+                            </Stack>
                         )
-                    })}
-                </Stack>
-            </PromiseVisualizer>
+                    )}
+                </PromiseVisualizer>
+            </Stack>
         </Stack>
     )
 };
