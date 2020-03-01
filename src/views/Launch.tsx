0f40fbd1f0cf1fa226be4f98f8318c646ccff082
Arlo Godfrey <arlo.godfrey@outlook.com>
2020-02-29 21:16:29 -0600

    Updated launch info and made style improvements

--- src/views/Launch.tsx
+++ src/views/Launch.tsx
@@ -7,6 +7,17 @@ import { IProject, GetLaunchProjects } from "../common/services/projects";
 import { ProjectCard } from "../components/ProjectCard";
 import { GetCurrentDiscordUser, IDiscordUser } from "../common/services/discord";
 import { PromiseVisualizer } from "../components/PromiseVisualizer";
+import styled from "styled-components";
+
+const PaddedProjectHolder = styled.div`
+    padding: 20px 0px 20px 0px;
+
+
+    @media screen and (min-width: 1200px) {
+            padding: 20px 100px 20px 100px;
+    }
+
+`;
 
 export const Launch = () => {
     const [launchProjects, setLaunchProjects] = React.useState<IProject[]>();
@@ -22,23 +33,28 @@ export const Launch = () => {
         <Stack tokens={{childrenGap: 25}} horizontalAlign="center">
             <Stack horizontal wrap style={{ boxShadow: Depths.depth16 }} maxWidth="1200px">
                 <Image width="100%" height="400px" src={Images.launchAppsHero} coverStyle={ImageCoverStyle.landscape} imageFit={ImageFit.cover} />
-                <Stack style={{margin: "20px"}}>
+                <Stack style={{margin: "20px 50px 20px 50px"}}>
 
                     <Text style={{fontFamily: "Segoe UI", fontSize: "30px", fontWeight: "lighter"}}>// Launch</Text>
 
                     <Text style={{marginTop: "10px", fontWeight: 500}} variant="xLarge">Learn, develop, and Launch together</Text>
-                    <Text style={{marginTop: "10px"}} variant="mediumPlus">Once a year, our community of Windows App developers join together to release their UWP-related projects. If you’re a developer, new or veteran, then we HIGHLY encourage you to join the server and register your app for the next Launch event!</Text>
-                    <Text style={{marginTop: "10px"}} variant="large">{ user ? "Go to your dashboard to get started" : ""}</Text>
+                    <Text style={{ marginTop: "10px" }} variant="mediumPlus">Once a year, our community of Windows App developers join together to release their UWP-related projects.</Text>
+                    <Text style={{ marginTop: "10px" }} variant="mediumPlus">This year, we have 14 public and 3 secret projects in total that will be launching together on June 7th!</Text>
                 </Stack>
             </Stack>
 
             <PromiseVisualizer promise={GetLaunchProjects(2020)} onResolve={setLaunchProjects} loadingMessage="Checking for Launch 2020 Participants">
-            <Text variant="xLarge">Launch 2020 Participants</Text>
-            <Stack horizontal wrap horizontalAlign="center" tokens={{childrenGap: 25}}>
-                {launchProjects && launchProjects.length && launchProjects.map((project, i) => 
-                    <ProjectCard key={i} project={project} />
-                )}
-            </Stack>
+                <PaddedProjectHolder>
+                    <Stack horizontalAlign="center" tokens={{ childrenGap: 15 }}>
+
+                        <Text variant="xLarge">Launch 2020 Participants</Text>
+                        <Stack horizontal wrap horizontalAlign="center" maxWidth={1800} tokens={{ childrenGap: 25 }}>
+                            {launchProjects && launchProjects.length && launchProjects.map((project, i) => 
+                                <ProjectCard key={i} project={project} />
+                                )}
+                        </Stack>
+                    </Stack>
+                </PaddedProjectHolder>
             </PromiseVisualizer>
         </Stack>
     );
