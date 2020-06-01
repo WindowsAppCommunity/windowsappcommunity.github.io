118e97046ff5eda9ccb516a6981ed915dd00e45e
Arlodotexe <arlo.godfrey@outlook.com>
2020-06-01 18:32:53 -0500

    Updates

---
+++ precache-manifest.aa031ae813ee2693b8f6bbf5b731c5af.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0bb02b6cb42c5acc1a3349e0717115f5",
+    "url": "/index.html"
+  },
+  {
+    "revision": "b52ac6dcb6df68117343",
+    "url": "/static/js/2.c3517f5b.chunk.js"
+  },
+  {
+    "revision": "c2baa7b55be0d43a8d73fb9336645d24",
+    "url": "/static/js/2.c3517f5b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d7664c2f60c429c08c09",
+    "url": "/static/js/main.a99a2995.chunk.js"
+  },
+  {
+    "revision": "3cac72df019adf119d43",
+    "url": "/static/js/runtime-main.0f04c299.js"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.aa5cc874414ac530ec272a91a5cdb562.js"
+  "/precache-manifest.aa031ae813ee2693b8f6bbf5b731c5af.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -35,5 +35,5 @@ workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
 workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
   
-  blacklist: [/^\/_/,/\/[^\/?]+\.[^\/]+$/],
+  blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
