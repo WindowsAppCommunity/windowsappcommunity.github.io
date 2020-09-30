9f28cbcb0bb802137915b0e7a80fe04a92ff3b13
Arlodotexe <arlo.godfrey@outlook.com>
2020-09-30 18:23:28 -0500

    Updates

---
+++ precache-manifest.2fb974d08d576958ef5449a54b0f4e1c.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7f32b77d999cfe8d4138404e8b64a685",
+    "url": "/index.html"
+  },
+  {
+    "revision": "2e1eae845adc1114fbff",
+    "url": "/static/js/2.a80c48fc.chunk.js"
+  },
+  {
+    "revision": "9b1eecf1fd93884583e55bdcc952417a",
+    "url": "/static/js/2.a80c48fc.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b928577dc0bbcc50214b",
+    "url": "/static/js/main.e5536112.chunk.js"
+  },
+  {
+    "revision": "3cac72df019adf119d43",
+    "url": "/static/js/runtime-main.0f04c299.js"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "./precache-manifest.173e63f3f2256e73fc0d271fa46b1531.js"
+  "/precache-manifest.2fb974d08d576958ef5449a54b0f4e1c.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("./index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
   
   blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
