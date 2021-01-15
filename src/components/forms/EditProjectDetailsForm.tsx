2c6bc27e50ad84eb47cdf4b509d01fd9eb18b09e
ItzLevvie <11600822+ItzLevvie@users.noreply.github.com>
2021-01-15 17:57:51 +0000

    Fix spelling mistake & capitalization
    * Changed ```Partipation``` to ```Participation```
    * Changed ```Github``` to ```GitHub```

--- src/components/forms/EditProjectDetailsForm.tsx
+++ src/components/forms/EditProjectDetailsForm.tsx
@@ -106,7 +106,7 @@ export const EditProjectDetailsForm = (props: IEditProjectDetailsFormProps) => {
                                 styles={{ root: { width: "100%" } }}
                                 onChange={(e: any, value: any) => setProjectRequest({ ...projectRequest, downloadLink: value })} />
 
-                            <TextField label="Github Link:"
+                            <TextField label="GitHub Link:"
                                 value={projectRequest.githubLink}
                                 styles={{ root: { width: "100%" } }}
                                 onChange={(e: any, value: any) => setProjectRequest({ ...projectRequest, githubLink: value })} />
@@ -133,7 +133,7 @@ export const EditProjectDetailsForm = (props: IEditProjectDetailsFormProps) => {
                                 checked={projectRequest.isPrivate}
                                 onChange={(e: any, value: any) => setProjectRequest({ ...projectRequest, isPrivate: value })} />
 
-                            <Checkbox label="Partipation in Launch 2021"
+                            <Checkbox label="Participation in Launch 2021"
                                 checked={projectRequest.awaitingLaunchApproval}
                                 onChange={(e: any, value: any) => setProjectRequest({ ...projectRequest, awaitingLaunchApproval: value })} />
 
