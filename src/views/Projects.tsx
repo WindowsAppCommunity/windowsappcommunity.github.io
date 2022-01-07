657db93541d239a9d060ea2ce5e6553ae6a32221
Arlo Godfrey <arlo.godfrey@outlook.com>
2022-01-07 23:19:40 +0000

    Allow admins to manage projects from projects page

--- src/views/Projects.tsx
+++ src/views/Projects.tsx
@@ -3,9 +3,27 @@ import { Stack, Text, FontIcon } from "office-ui-fabric-react";
 import { GetAllProjects, IProject } from "../common/services/projects";
 import { ProjectCard } from "../components/ProjectCard";
 import { PromiseVisualizer } from "../components/PromiseVisualizer";
+import { GetCurrentDiscordUser, GetUserRoles } from "../common/services/discord";
 
-export const Projects: React.StatelessComponent = () => {
+export const Projects = () => {
   const [state, setState] = React.useState<IProject[]>();
+  const [isAdmin, setIsAdmin] = React.useState<boolean>(false);
+
+  React.useEffect(() => {
+    setupUserRoles();
+  }, []);
+
+  async function setupUserRoles() {
+    var currentUser = await GetCurrentDiscordUser();
+    if (!currentUser)
+      return;
+
+    var currentUserRoles = await GetUserRoles(currentUser);
+    if (!currentUserRoles)
+      return;
+
+    setIsAdmin(currentUserRoles.filter(x => x == "Admin").length > 0);
+  }
 
   return (
     /* Todo: Add a header with brief explanation of the below */
@@ -13,13 +31,13 @@ export const Projects: React.StatelessComponent = () => {
       <PromiseVisualizer promise={GetAllProjects()} onResolve={setState} loadingMessage='Loading Projects...' loadingStyle={{ padding: 25 }} errorStyle={{ padding: 25 }}>
         {state && (
           state.length > 0 ? state.sort((a, b) => a.appName.localeCompare(b.appName)).map((project, i) => (
-            <ProjectCard key={i} onProjectRemove={project => setState(state.filter(p => p.appName !== project.appName))} project={project}></ProjectCard>
+            <ProjectCard key={i} onProjectRemove={project => setState(state.filter(p => p.appName !== project.appName))} project={project} editable={isAdmin}></ProjectCard>
           )) : (
-              <Stack horizontalAlign="center" >
-                <FontIcon iconName="sad" style={{ fontSize: 55 }}></FontIcon>
-                <Text variant="xLarge">No projects, yet</Text>
-              </Stack>
-            )
+            <Stack horizontalAlign="center" >
+              <FontIcon iconName="sad" style={{ fontSize: 55 }}></FontIcon>
+              <Text variant="xLarge">No projects, yet</Text>
+            </Stack>
+          )
         )}
       </PromiseVisualizer>
     </Stack>
